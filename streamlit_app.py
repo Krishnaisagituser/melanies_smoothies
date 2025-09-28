ec711b760adaf7da592b605ec8fb781c6a56a9e0
Krishnaisagituser <156786337+Krishnaisagituser@users.noreply.github.com>
2025-09-28 12:44:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ ingredients_List = st.multiselect(
     , my_dataframe
     ,max_selections=5
 )
-if ingredients_List and Name:
+if ingredients_List:
     
     # st.write(ingredients_List)
     # st.text(ingredients_List)
